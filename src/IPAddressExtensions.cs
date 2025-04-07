6ba38169348eb44792d814927b399db76c7d9ff7
Victor Nova <victor@borg.games>
2025-04-07 15:38:56 -0700

    + IPAddress.IsYggdrasil extension method

---
+++ src/IPAddressExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Yggdrasil;
+
+using System.Net;
+using System.Net.Sockets;
+
+public static class IPAddressExtensions {
+    const byte YGGDRASIL_NETWORK_BYTE = 0x02;
+    const byte YGGDRASIL_PREFIX_MASK = 0xFE;
+
+    public static bool IsYggdrasil(this IPAddress ip) {
+        if (ip is null) throw new ArgumentNullException(nameof(ip));
+
+        return ip.AddressFamily == AddressFamily.InterNetworkV6
+            && (ip.GetAddressBytes()[0] & YGGDRASIL_PREFIX_MASK) == YGGDRASIL_NETWORK_BYTE;
+    }
+}
---
+++ test/IPTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Yggdrasil;
+
+using System.Net;
+
+public class IPTests {
+    static readonly IPAddress PublicV6 = IPAddress.Parse("2607:f8b0:400a:801::200e");
+    static readonly IPAddress LinkLocalV6 = IPAddress.Parse("fe80::9408:9e72:f01b:3f03%31");
+    static readonly IPAddress YggdrasilV6_1 = IPAddress.Parse("324:71e:281a:9ed3::41");
+    static readonly IPAddress YggdrasilV6_2 = IPAddress.Parse("200:6f99:2afe:41fd:fc3b:b1f7:af9d:f3a0");
+
+    [Fact]
+    public void IsYggdrasil() {
+        Assert.False(PublicV6.IsYggdrasil());
+        Assert.False(LinkLocalV6.IsYggdrasil());
+
+        Assert.False(IPAddress.IPv6Any.IsYggdrasil());
+        Assert.False(IPAddress.IPv6Loopback.IsYggdrasil());
+        Assert.False(IPAddress.IPv6None.IsYggdrasil());
+
+        Assert.True(YggdrasilV6_1.IsYggdrasil());
+        Assert.True(YggdrasilV6_2.IsYggdrasil());
+    }
+}
